2ede0ad9dad5a3a1ce8c3fa541fa4fe45e5a9882
Hien Nguyen <hnguy179@gmail.com>
2021-03-29 16:59:45 -0700

    Listened for input events on photoUrl input. #1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 /* global data */
 /* exported data */
+var $photoUrlInput = document.getElementById('photo-url');
+var $entryImage = document.querySelector('.entry-image');
+
+function updateEntryImage() {
+  $entryImage.src = $photoUrlInput.value;
+}
+
+$photoUrlInput.addEventListener('input', updateEntryImage);
