de115bb6a4cb2dd86e7b6625c7512ddf6bcd0d2e
Hien Nguyen <hnguy179@gmail.com>
2021-03-30 12:37:07 -0700

    Resolved issues

--- js/data.js
+++ js/data.js
@@ -1,9 +1,4 @@
 /* exported data */
-var $form = document.getElementById('form');
-var $photoUrl = document.getElementById('photo-url');
-var $title = document.getElementById('title');
-var $notes = document.getElementById('notes');
-
 var data = {
   view: 'entry-form',
   entries: [],
@@ -16,20 +11,7 @@ if (previousDataJSON) {
   data = JSON.parse(previousDataJSON);
 }
 
-function addEntry(event) {
-  var entries = {
-    title: $title.value,
-    photoURL: $photoUrl.value,
-    notes: $notes.value,
-    entryId: data.nextEntryId
-  };
-
-  data.nextEntryId++;
-  data.entries.unshift(entries);
-  $form.reset();
-
+window.addEventListener('beforeunload', function (event) {
   var dataJSON = JSON.stringify(data);
   localStorage.setItem('javascript-local-storage', dataJSON);
-}
-
-$form.addEventListener('submit', addEntry);
+});
--- js/main.js
+++ js/main.js
@@ -2,9 +2,26 @@
 /* exported data */
 var $photoUrl = document.getElementById('photo-url');
 var $previewImage = document.querySelector('.preview-image');
+var $form = document.getElementById('form');
+var $title = document.getElementById('title');
+var $notes = document.getElementById('notes');
 
 function updateEntryImage() {
   $previewImage.src = $photoUrl.value;
 }
 
+function addEntry(event) {
+  var entries = {
+    title: $title.value,
+    photoURL: $photoUrl.value,
+    notes: $notes.value,
+    entryId: data.nextEntryId
+  };
+
+  data.nextEntryId++;
+  data.entries.unshift(entries);
+  $form.reset();
+}
+
 $photoUrl.addEventListener('input', updateEntryImage);
+$form.addEventListener('submit', addEntry);
