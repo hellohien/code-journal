4c46a3f7e08181123f85a203aa9450327f049152
Hien Nguyen <hnguy179@gmail.com>
2021-03-29 18:41:30 -0700

    Listened for submit events. #1

--- js/data.js
+++ js/data.js
@@ -1,4 +1,9 @@
 /* exported data */
+var $form = document.getElementById('form');
+var $photoUrl = document.getElementById('photo-url');
+var $entryImage = document.querySelector('.entry-image');
+var $title = document.getElementById('title');
+var $notes = document.getElementById('notes');
 
 var data = {
   view: 'entry-form',
@@ -6,3 +11,27 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var previousDataJSON = localStorage.getItem('javascript-local-storage');
+if (previousDataJSON) {
+  data = JSON.parse(previousDataJSON);
+}
+
+function addEntry(event) {
+  var entries = {
+    title: $title.value,
+    photoURL: $photoUrl.value,
+    notes: $notes.value,
+    entryId: data.nextEntryId
+  };
+
+  data.nextEntryId++;
+  data.entries.unshift(entries);
+  $entryImage.src = 'images/placeholder-image-square.jpg';
+  $form.reset();
+
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', dataJSON);
+}
+
+$form.addEventListener('submit', addEntry);
