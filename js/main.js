ff7a98d683c25f3f7bdb31a16e4ee0fa1e7a274a
Hien Nguyen <hnguy179@gmail.com>
2021-03-30 17:00:41 -0700

    Added a function to take in an entry object and appended the dom tree to the page. #2

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var $previewImage = document.querySelector('.preview-image');
 var $form = document.getElementById('form');
 var $title = document.getElementById('title');
 var $notes = document.getElementById('notes');
+var $entriesContainer = document.querySelector('.entries-container');
 
 function updateEntryImage() {
   $previewImage.src = $photoUrl.value;
@@ -23,5 +24,35 @@ function addEntry(event) {
   $form.reset();
 }
 
+function entryList(entryObj) {
+  var $list = document.createElement('li');
+  $list.setAttribute('class', 'row entry-list');
+
+  var $image = document.createElement('img');
+  $image.setAttribute('class', 'column-half');
+  $image.setAttribute('src', entryObj.photoURL);
+  $image.setAttribute('alt', 'Preview Image');
+  $list.appendChild($image);
+
+  var $div = document.createElement('div');
+  $div.setAttribute('class', 'column-half');
+  $list.appendChild($div);
+
+  var $heading = document.createElement('h2');
+  $heading.textContent = entryObj.title;
+  $div.appendChild($heading);
+
+  var $paragraph = document.createElement('p');
+  $paragraph.textContent = entryObj.notes;
+  $div.appendChild($paragraph);
+
+  return $list;
+}
+
+for (var i = 0; i < data.entries.length; i++) {
+  $entriesContainer.appendChild(entryList(data.entries[i]));
+}
+
 $photoUrl.addEventListener('input', updateEntryImage);
 $form.addEventListener('submit', addEntry);
+$entriesContainer.addEventListener('DOMContentLoaded', entryList);
